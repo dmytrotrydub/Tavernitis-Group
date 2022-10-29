0dcdac78904baf2cddcb26ee06c8a2a42cee583a
dmytrotrydub <tridub.d87@gmail.com>
2022-10-29 19:24:26 -0400

    header sticky created for Desktop-size

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,36 @@
 import './Header.scss';
 import nav from '../../Assets/Images/Images/svg/Taverniti-Nav.png';
+import { useState, useEffect } from 'react';
+import { useScrollDirection } from '../../Helper/ScrollDirectionHook';
 
 const Header = () => {
+
+  const scrollDirection = useScrollDirection();
+
   return (
-    <header className='header'>
+    <header className={`header ${scrollDirection === 'down' ? "hide" : "show"} `}>
       <button type='button' id='nav-button'>
         <img src={nav} id='nav-image' alt='navigation' />
       </button>
+      <div className='nav'>
+        <a className='nav__option' href='#'>
+          HOME
+        </a>
+        <a className='nav__option' href='#'>
+          HISTORY OF THE BRAND
+        </a>
+        <a className='nav__option' href='#'>
+          GALLERY
+        </a>
+        <a className='nav__option' href='#'>
+          US IN MEDIA
+        </a>
+        <a className='nav__option' href='#'>
+          SIGNATURE DISHES
+        </a>
+      </div>
     </header>
   );
-};
+};;
 
 export default Header;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,30 +5,51 @@ import rosinaLogo from '../../Assets/Images/Images/LOGOS/Rosina-Logo.jpg';
 import tavernitiGroupLogo from '../../Assets/Images/Images/LOGOS/Taverniti-Group-Logo(main).jpg';
 import tavsLogo from '../../Assets/Images/Images/LOGOS/Tavs-Logo.jpg';
 import tavernitiNorthLogo from '../../Assets/Images/Images/LOGOS/Tavernitis-North-Logo.jpg';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
+
+
+
+
   return (
     <>
       <Header />
       <div className='main-logo'>
         <div className='main-logo__restList'>
           <div className='main-logo__resto-card'>
-            <img src={tavernitiLogo} className='main-logo__resto' alt='taverniti-restaurant' />
+            {isHovering === false ? (
+              <img
+                src={tavernitiLogo}
+                className='main-logo__resto'
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+                alt='taverniti-restaurant'
+              />
+            ) : null}
+
+            <div>
+              
+              {/* <Link to='http://www.tavernitis.ca/' /> */}
+            </div>
           </div>
-          <div className='main-logo__resto-card'>
-            <img src={rosinaLogo} className='main-logo__resto' alt='rosina-restaurant' />
-          </div>
-        </div>
-        <img src={tavernitiGroupLogo} alt='group-logo' />
-        <div className='main-logo__secRestList'>
           <div className='main-logo__resto-card'>
             <img src={tavsLogo} className='main-logo__resto' alt='tavs-logo' />
           </div>
+        </div>
+        <img id='main-logo' src={tavernitiGroupLogo} alt='group-logo' />
+        <div className='main-logo__secRestList'>
+          <div className='main-logo__resto-card'>
+            <img src={tavernitiNorthLogo} className='main-logo__resto' alt='taverniti-north-logo' />
+          </div>
           <div className='main-logo__resto-card'>
             <img
-              src={tavernitiNorthLogo}
+              src={rosinaLogo}
               className='main-logo__resto'
-              alt='taverniti-north-logo'
+              id='rosina'
+              alt='rosina-restaurant'
             />
           </div>
         </div>
